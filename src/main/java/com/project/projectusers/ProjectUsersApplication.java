4fb4c12b3de17ac20722d8426a2ae1ddc5880a97
KateM3d <ekaterina8medvedeva@gmail.com>
2023-02-24 16:17:14 -0500

    initial commit

---
+++ src/main/java/com/project/projectusers/ProjectUsersApplication.java
@@ -0,0 +1,13 @@
+package com.project.projectusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectUsersApplication.class, args);
+    }
+
+}
